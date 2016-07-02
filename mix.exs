9176838c81e0429f62212d714597352c7b03c494
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2016-07-02 20:28:43 +0530

    Use escript to run the program from command line
     - Rename run function to main function to use in escript

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.Mixfile do
   def project do
     [
       app: :issues,
+      escript: escript_config,
       version: "0.0.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
@@ -15,23 +16,37 @@ defmodule Issues.Mixfile do
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
+
   def application do
-    [applications: [:logger, :httpoison]]
+    [
+      applications: [:logger, :httpoison]
+    ]
   end
 
   # Dependencies can be Hex packages:
   #
-  #   {:mydep, "~> 0.3.0"}
+  #   {
+  #     :mydep, "~> 0.3.0"
+  #   }
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #   {
+  #     :mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"
+  #   }
   #
   # Type "mix help deps" for more examples and options
+
   defp deps do
     [
       httpoison: "~> 0.8.3",
       poison: "~> 2.1"
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
